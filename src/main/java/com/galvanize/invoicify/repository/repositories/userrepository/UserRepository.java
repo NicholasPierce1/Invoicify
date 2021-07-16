362a4af17c09cc53175594974b58b837032af604
kiz <aiquizon@live.com>
2021-07-16 13:55:48 -0400

    added native query to custom user count query

--- src/main/java/com/galvanize/invoicify/repository/repositories/userrepository/UserRepository.java
+++ src/main/java/com/galvanize/invoicify/repository/repositories/userrepository/UserRepository.java
@@ -15,7 +15,7 @@ public interface UserRepository extends JpaRepository<UserDataAccess, Long> {
 	//add special method to find users by username
 	Optional<UserDataAccess> findByUsername(String username);
 
-	@Query("SELECT count(*) from app_user WHERE u.userName = ?1" )
+	@Query(value = "SELECT count(*) from app_user WHERE username = ?1",nativeQuery = true)
 	int countUsersByUserName(String username);
 
 }
