30e8b1681afc4af0e39ff8405a90e44734ae891e
abutler6588 <abutler6588@gmail.com>
2021-07-20 10:45:35 -0500

    company user stories finished

--- src/main/java/com/galvanize/invoicify/controllers/CompanyController.java
+++ src/main/java/com/galvanize/invoicify/controllers/CompanyController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/app/company")
+@RequestMapping("/api/company")
 public class CompanyController  {
 
 //    @Autowired
