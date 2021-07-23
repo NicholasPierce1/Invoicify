5b881a884904e2c0598815406f867f5ea432b44b
Berkant Bulut <berkantbulut91@gmail.com>
2021-07-23 13:04:37 -0400

    documentation for CompanyRepository

--- src/main/java/com/galvanize/invoicify/repository/repositories/companyrepository/CompanyRepository.java
+++ src/main/java/com/galvanize/invoicify/repository/repositories/companyrepository/CompanyRepository.java
@@ -9,6 +9,16 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * <h2>
+ *     This interface extends the JPARepository, which takes in a CompanyDataAccess Object, along
+ *      with its serialized Long id. This repository defines a method that finds a company by
+ *      its name. Important to note that it inherits the methods from the JPARepository,
+ *      so this interface is not confined to just the method that is present. Most of
+ *      the methods called on by the CompanyRepository actually are inherited from
+ *      parent interface
+ * </h2>
+ */
 @Repository
 public interface CompanyRepository extends JpaRepository<CompanyDataAccess, Long> {
 
