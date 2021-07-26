1b80518ad4ab0bc331ff92c6d75d608ad8c95b12
Nicholas Pierce <nickpierce7898@gmail.com>
2021-07-26 11:13:45 -0500

    removed unused files and packages

--- src/main/java/com/galvanize/invoicify/repository/repositories/flatfeebillingrecord/FlatFeeBillingRecordManagerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.galvanize.invoicify.repository.repositories.flatfeebillingrecord;
-
-import com.galvanize.invoicify.repository.dataaccess.FlatFeeBillingRecordDataAccess;
-
-public interface FlatFeeBillingRecordManagerRepository {
-
-    // stubs
-
-}
