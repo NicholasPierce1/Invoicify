841ff143601060828f1b495e302763b2598da91c
kiz <aiquizon@live.com>
2021-07-28 10:15:40 -0400

    added more comments to invoicerepository and invoicerepositoryimpl

--- src/main/java/com/galvanize/invoicify/repository/repositories/invoicerepository/InvoiceRepository.java
+++ src/main/java/com/galvanize/invoicify/repository/repositories/invoicerepository/InvoiceRepository.java
@@ -4,6 +4,12 @@ import com.galvanize.invoicify.repository.dataaccess.InvoiceDataAccess;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * <h2>
+ *     This interface extends the JPARepository, which takes in a InvoiceDataAccess Object, along
+ *      with its serialized Long id. This method implements InvoiceRepositoryCustom to run custom queries and return a cutom invoice response.
+ * </h2>
+ */
 @Repository
 public interface InvoiceRepository extends JpaRepository<InvoiceDataAccess, Long>, InvoiceRepositoryCustom {
 
--- src/main/java/com/galvanize/invoicify/repository/repositories/invoicerepository/InvoiceRepositoryCustom.java
+++ src/main/java/com/galvanize/invoicify/repository/repositories/invoicerepository/InvoiceRepositoryCustom.java
@@ -5,9 +5,29 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ *<p>
+ *     this interface is being implemented by the invoice manager(InvoiceRepositoryImpl.java) and InvoiceRepository to connect both and use custom queries.
+ *</p>
+ */
 public interface InvoiceRepositoryCustom {
 
+    /**
+     * fetchInvoice interface method for fetching a single invoice after creation
+     * @param invoiceId - this is passed in from the client.
+     * @param recordIds - list of record ids that were passed in from the client.
+     * @return InvoiceDataAccess
+     */
     InvoiceDataAccess fetchInvoice(long invoiceId, List<Long> recordIds);
+
+    /**
+     * <p>
+     *     fetch all invoices don't require params so the parameters don't contain any valuable data.
+     * </p>
+     * @param invoiceId - this is usually 0
+     * @param recordIds - tihs is usually empty
+     * @return list of all invoices.
+     */
     List<InvoiceDataAccess> fetchInvoices(long invoiceId, List<Long> recordIds);
 
 }
